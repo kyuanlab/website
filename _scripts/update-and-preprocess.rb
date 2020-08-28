fe3f7619a1723204668e1d4689b9e552ed3e8da2
Ke Yuan <ke.yuan.09@gmail.com>
2020-08-28 18:03:27 +0100

    test

--- _scripts/update-and-preprocess.rb
+++ _scripts/update-and-preprocess.rb
@@ -11,10 +11,10 @@ config["projects"].each do |repo|
 	if !Dir.exists?(name)								# clone project repo
 		`git clone https://github.com/#{repo}.git`
 	end
-	Dir.chdir($basedir + "/projects/" + name)			# drop into blotter dir	
-	`git clean -f`										# remove untracked files, but keep directories
-	`git reset --hard HEAD`								# bring back to head state
-	`git pull origin master`							# git pull				
+	# Dir.chdir($basedir + "/projects/" + name)			# drop into blotter dir	
+	# `git clean -f`										# remove untracked files, but keep directories
+	# `git reset --hard HEAD`								# bring back to head state
+	# `git pull origin master`							# git pull				
 end
 
 Dir.chdir($basedir)
@@ -23,7 +23,6 @@ Dir.chdir($basedir)
 
 config["projects"].each do |repo|
 	name = repo.split('/').drop(1).join('')		
-	Dir.chdir($basedir + "/projects")			
 	Dir.chdir($basedir + "/projects/" + name)			
 	`shopt -s extglob`
 	`rm -rf !(*.md)`	
